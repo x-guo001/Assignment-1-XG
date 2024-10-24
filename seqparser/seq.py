4990e4aa45827c3a3f0faa4a95cac44b81c32220
x-guo001 <182980663+x-guo001@users.noreply.github.com>
2024-10-23 22:01:47 -0700

    addmore comments to seq.py

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -6,7 +6,9 @@ def transcribe(seq: str) -> str:
     TODO: transcribes DNA to RNA by generating
     the complement sequence with T -> U replacement
     """
+    # make a DNA to RNA conversion dictionary 
     dna_to_rna = {'A':'U', 'T':'A', 'C':'G', 'G':'C'}
+    # transcribe the DNA sequence to RNA sequence following the converstion rule outlined above
     def rna_generator():
         for dna in seq:
             if dna in dna_to_rna:
@@ -21,5 +23,6 @@ def reverse_transcribe(seq: str) -> str:
     the strand
     """
     rna = transcribe(seq)
+    #reversed the RNA sequence using 'reversed' function
     rna_reverse = ''.join(reversed(rna))
     return rna_reverse
